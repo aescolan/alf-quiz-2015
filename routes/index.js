80f2565b4a4a59dd45da28d305cb9a265b7cd6b8
aescolan <aescolan@ull.edu.es>
2015-07-07 20:33:12 +0100

    9 - Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ var creditosController = require('../controllers/creditos_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
-
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
 router.get('/creditos/autor', creditosController.autor);
+
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
