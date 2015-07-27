71f56a86744a9065ec6ce6ae112e079290154d22
aescolan <aescolan@ull.edu.es>
2015-07-27 13:06:45 +0100

    18- Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
-
+router.param('commentId', commentController.load); // autoload :commentId
 
 //Definicion de rutas de sesion
 router.get('/login', sessionController.new);      // formulario login
@@ -28,14 +28,15 @@ router.get('/logout', sessionController.destroy); // destruir sesión
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
-router.get('/quizes/:new', 					quizController.new);
-router.post('/quizes/:create', 				quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
-router.put('/quizes/:quizId(\\d+)', 		quizController.update);
-router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
+router.get('/quizes/:new', 					      sessionController.loginRequired, quizController.new);
+router.post('/quizes/:create', 				    sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		  sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
 // RUTAS COMENTARIOS - Crear comentario
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
